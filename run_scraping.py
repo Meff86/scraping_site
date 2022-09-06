4db2a38227bc17b07d1fc59a1ff6912bfa58c8e0
Meff86 <meff86@list.ru>
2022-09-07 01:05:16 +0300

    Добвил модель Ошибок и Сбора Url, добавил в модель юзера предпочтения языка и возмжность рассылки, а так же реализовал функции для связывания предопчтений и получения урлов к ним

--- run_scraping.py
+++ run_scraping.py
@@ -18,37 +18,45 @@ from scraping.parsers import *
 
 User = get_user_model()
 
-parsers = ((hh, 'https://hh.ru/search/vacancy?area=1&search_field=name&text=python&no_magic=true&L_save_area=true&items_on_page=50' ),
-           (habr, 'https://career.habr.com/vacancies?city_id=678&q=python&type=all'))
+parsers = ((hh, 'hh' ),
+           (habr, 'habr'))
 
+jobs, errors = [], []
 def get_settings():
-    qs =User.objects.filter(send_email=True).values()
+    qs = User.objects.filter(send_email=True).values()
     settings_lst = set((q['city_id'], q['language_id']) for q in qs)
     return settings_lst
 
 def get_urls(_settings):
     qs = Url.objects.all().values()
-    url_dct = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    url_dict = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
     urls = []
     for pair in _settings:
-        tmp = {}
-        tmp['city'] = pair[0]
-        tmp['language'] = pair[1]
-        tmp['url_data'] = url_dct[pair]
-        urls.append(tmp)
+        if pair in url_dict:
+            tmp = {}
+            tmp['city'] = pair[0]
+            tmp['language'] = pair[1]
+            url_data = url_dict.get(pair)
+            if url_data:
+                tmp['url_data'] = url_dict.get(pair)
+                urls.append(tmp)
     return urls
 
-city = City.objects.filter(slug='moskva').first()
-language = Language.objects.filter(slug='python').first()
+#city = City.objects.filter(slug='moskva').first()
+#language = Language.objects.filter(slug='python').first()
+settings = get_settings()
+url_list = get_urls(settings)
 
-jobs, errors = [], []
-for func, url in parsers:
-    j, e = func(url)
-    jobs += j
-    errors += e
+for data in url_list:
+    for func, key in parsers:
+        url = data['url_data'][key]
+        j, e = func(url, city=data['city'], language=data['language'])
+        jobs += j
+        errors += e
+
 
 for job in jobs:
-    v = Vacancy(**job, city=city, language=language)
+    v = Vacancy(**job,)
     try:
         v.save()
     except DatabaseError:
@@ -56,11 +64,3 @@ for job in jobs:
 if errors:
     er = Error(data=errors).save()
 
-
-
-
-
-
-#h = codecs.open('work.txt', 'w', 'utf-8')
-#h.write(str(jobs))
-#h.close()
--- scraping/parsers.py
+++ scraping/parsers.py
@@ -15,63 +15,67 @@ headers = [{'User-Agent': 'Mozilla/5.0 (Windows NT 5.1; rv:47.0) Gecko/20100101 
 
 
 
-def hh(url):
+def hh(url, city=None, language=None):
     jobs = []
     errors = []
-    resp = requests.get(url, headers=headers[randint(0, 2)])
-    if resp.status_code == 200:
-        soup = BS(resp.content, 'html.parser')
-        new_jobs = soup.find('div', attrs={'class': 'no-content__title'})
-        if not new_jobs:
-            main_div = soup.find('div', id='a11y-main-content')
-            if main_div:
-                div_lst = main_div.find_all('div', attrs={'class': 'serp-item'})
-                for div in div_lst:
-                    title = div.find('h3')
-                    href = title.a['href']
-                    content = div.find('div', attrs={'class': 'g-user-content'})
-                    company = div.find(attrs={'class': 'bloko-link_kind-tertiary'})
-                    jobs.append({'title': title.text, 'url': href, 'description': content.text, 'company': company.text})
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, 2)])
+        if resp.status_code == 200:
+            soup = BS(resp.content, 'html.parser')
+            new_jobs = soup.find('div', attrs={'class': 'no-content__title'})
+            if not new_jobs:
+                main_div = soup.find('div', id='a11y-main-content')
+                if main_div:
+                    div_lst = main_div.find_all('div', attrs={'class': 'serp-item'})
+                    for div in div_lst:
+                        title = div.find('h3')
+                        href = title.a['href']
+                        content = div.find('div', attrs={'class': 'g-user-content'})
+                        company = div.find(attrs={'class': 'bloko-text'})
+                        jobs.append({'title': title.text, 'url': href, 'description': content.text, 'company': company.text,
+                                     'city_id': city, 'language_id': language})
+                else:
+                    errors.append({'url': url, 'title': "Div does not exist"})
             else:
-                errors.append({'url': url, 'title': "Div does not exist"})
+                errors.append({'url': url, 'title': 'Page is empty'})
         else:
-            errors.append({'url': url, 'title': 'Page is empty'})
-    else:
-        errors.append({'url': url, 'title': "Page do not response"})
+            errors.append({'url': url, 'title': "Page do not response"})
 
     return jobs, errors
 
-def habr(url):
+def habr(url, city=None, language=None):
     jobs = []
     errors = []
     domain = 'https://career.habr.com'
-    resp = requests.get(url, headers=headers[randint(0, 2)])
-    if resp.status_code == 200:
-        soup = BS(resp.content, 'html.parser')
-        new_jobs = soup.find('div', attrs={'class': 'no-content__title'})
-        if not new_jobs:
-            main_div = soup.find('div', attrs={'class': 'section-group--gap-medium'})
-            if main_div:
-                div_lst = main_div.find_all('div', attrs={'class': 'vacancy-card'})
-                for div in div_lst:
-                    title = div.find('div', attrs={'class': 'vacancy-card__title'})
-                    href = title.a['href']
-                    content = div.find('div', attrs={'class': 'vacancy-card__skills'})
-                    company = div.find(attrs={'class': 'vacancy-card__company-title'})
+    if url:
+        resp = requests.get(url, headers=headers[randint(0, 2)])
+        if resp.status_code == 200:
+            soup = BS(resp.content, 'html.parser')
+            new_jobs = soup.find('div', attrs={'class': 'no-content__title'})
+            if not new_jobs:
+                main_div = soup.find('div', attrs={'class': 'section-group--gap-medium'})
+                if main_div:
+                    div_lst = main_div.find_all('div', attrs={'class': 'vacancy-card'})
+                    for div in div_lst:
+                        title = div.find('div', attrs={'class': 'vacancy-card__title'})
+                        href = title.a['href']
+                        content = div.find('div', attrs={'class': 'vacancy-card__skills'})
+                        company = div.find(attrs={'class': 'vacancy-card__company-title'})
 
-                    jobs.append({'title': title.text, 'url': domain + href, 'description': content.text, 'company': company.text})
+                        jobs.append({'title': title.text, 'url': domain + href, 'description': content.text, 'company': company.text,
+                                     'city_id': city, 'language_id': language})
+                else:
+                    errors.append({'url': url, 'title': "Div does not exist"})
             else:
-                errors.append({'url': url, 'title': "Div does not exist"})
+                errors.append({'url': url, 'title': 'Page is empty'})
         else:
-            errors.append({'url': url, 'title': 'Page is empty'})
-    else:
-        errors.append({'url': url, 'title': "Page do not response"})
+            errors.append({'url': url, 'title': "Page do not response"})
 
     return jobs, errors
 
-if __name__ == '__main__':
-    url = 'https://career.habr.com/vacancies?city_id=678&q=python&type=all'
-    jobs, errors = habr(url)
-    h = codecs.open('work.txt', 'w', 'utf-8')
-    h.write(str(jobs))
-    h.close()
+#if __name__ == '__main__':
+#    url = 'https://career.habr.com/vacancies?city_id=678&q=python&type=all'
+#   jobs, errors = habr(url)
+#    h = codecs.open('work.txt', 'w', 'utf-8')
+#   h.write(str(jobs))
+#   h.close()
