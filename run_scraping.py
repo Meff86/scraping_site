141ca27a06bb50609ad84fa6e0b0f4c565e96268
Meff86 <meff86@list.ru>
2022-09-06 14:23:34 +0300

    Добвил модель Ошибок и Сбора Url, добавил в модель юзера предпочтения языка и возмжность рассылки, а так же реализовал функции для связывания предопчтений и получения урлов к ним

--- run_scraping.py
+++ run_scraping.py
@@ -1,6 +1,7 @@
 import codecs
 import os, sys
 
+from django.contrib.auth import get_user_model
 from django.db import DatabaseError
 
 proj = os.path.dirname(os.path.abspath('manage.py'))
@@ -11,12 +12,31 @@ import django
 django.setup()
 
 
-from scraping.models import City, Vacancy, Language
+from scraping.models import City, Vacancy, Language, Error, Url
 
 from scraping.parsers import *
 
+User = get_user_model()
+
 parsers = ((hh, 'https://hh.ru/search/vacancy?area=1&search_field=name&text=python&no_magic=true&L_save_area=true&items_on_page=50' ),
            (habr, 'https://career.habr.com/vacancies?city_id=678&q=python&type=all'))
+
+def get_settings():
+    qs =User.objects.filter(send_email=True).values()
+    settings_lst = set((q['city_id'], q['language_id']) for q in qs)
+    return settings_lst
+
+def get_urls(_settings):
+    qs = Url.objects.all().values()
+    url_dct = {(q['city_id'], q['language_id']): q['url_data'] for q in qs}
+    urls = []
+    for pair in _settings:
+        tmp = {}
+        tmp['city'] = pair[0]
+        tmp['language'] = pair[1]
+        tmp['url_data'] = url_dct[pair]
+        urls.append(tmp)
+    return urls
 
 city = City.objects.filter(slug='moskva').first()
 language = Language.objects.filter(slug='python').first()
@@ -33,6 +53,8 @@ for job in jobs:
         v.save()
     except DatabaseError:
         pass
+if errors:
+    er = Error(data=errors).save()
 
 
 
--- scraping/models.py
+++ scraping/models.py
@@ -1,6 +1,10 @@
 from django.db import models
+import jsonfield
 
 from scraping.utils import from_cyrillic_to_eng
+
+def default_urls():
+    return {"hh": "", "habr": ""}
 
 
 class City(models.Model):
@@ -52,4 +56,17 @@ class Vacancy(models.Model):
 
     def __str__(self):
         return self.title
-# Create your models here.
+
+class Error(models.Model):
+    timestamp = models.DateField(auto_now_add=True)
+    data = jsonfield.JSONField()
+
+class Url(models.Model):
+    city = models.ForeignKey('City', on_delete=models.CASCADE,
+                             verbose_name='Город')
+    language = models.ForeignKey('Language', on_delete=models.CASCADE,
+                                 verbose_name='Язык программирования')
+    url_data = jsonfield.JSONField(default=default_urls)
+
+    class Meta:
+        unique_together = ("city", "language")
